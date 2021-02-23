22719fcc271a17fcfa5e4db2ce3de5b9b5813c79
Victoriano Giralt <victoriano@uma.es>
2021-02-24 00:46:39 +0100

    Object methods for saving and deleting added

--- models.py
+++ models.py
@@ -14,6 +14,7 @@ from django.conf import settings
 from django.core.mail import send_mail, EmailMessage
 from django.core.signing import TimestampSigner
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 from django_countries.fields import CountryField
 from django.contrib.auth.models import AbstractUser
 AbstractUser._meta.get_field('email')._unique = True
@@ -21,6 +22,7 @@ AbstractUser._meta.get_field('email')._unique = True
 from urllib import parse
 import uuid
 import requests
+import calendar
 
 # Create your models here.
 
@@ -50,8 +52,8 @@ class HEI(models.Model):
     termstart = models.SmallIntegerField(
                            choices = MONTHS,
                            verbose_name = _('Term start month'),
-                           help_text = _('Month the term starts on.'
-                                         ' Used for expiry date calculations.'))
+                           help_text = _(
+                'Month the term starts on, for expiration date calculations.'))
     url = models.URLField(max_length = 256,
                            db_index = True,
                            blank = True,
@@ -88,17 +90,43 @@ class HEI(models.Model):
     class Meta:
         verbose_name = _('Higher Education Institution')
         verbose_name_plural = _('Higher Education Institutions')
+        ordering = ['country', 'name']
 
 
     def __str__(self):
         return '{0.name} - {0.sho} - {0.pic}'.format(self)
+
+    def delete(self, *args, **kwargs):
+        """
+        Check that persons that entered the system via the deleted HEI
+        get associated to other managing person if they have enrolments
+        in other HEI 
+        """
+        for student in self.students.all():
+            card = student.person.cards.all().exclude(hei=self)
+            if card is not None:
+                # Person is enrolled in other HEI at least
+                # Pass control to the manager of first HEI
+                person = student.person
+                person.managedBy = card.manager.person
+                person.save()
+        # Officers may be enrolled as students in other HEIs
+        for officer in self.officers.all():
+            card = officer.person.cards.all().exclude(hei=self)
+            if card is not None:
+                # Person is enrolled in other HEI at least
+                # Pass control to the manager of first HEI
+                person = officer.person
+                person.managedBy = card.manager.person
+                person.save()
+        super(HEI, self).delete(*args, **kwargs)
+            
 
     def generate_esc_code(self):
         """
         Returns an ESC for a student enroled in the HEI
         It is HEI specific, thus, we use a HEI object level method
         """
-        import uuid
         # The machine ID is used by ESC in case there are more than system
         # producing ESC cards for an instituion, usually it is just one
         machineId = settings.__dict__.get('MACHINEID','001')
@@ -121,6 +149,7 @@ class HEI(models.Model):
             def __init__(self):
                 self.status_code = 500
                 self.url = 'Error'
+                self.json = {}
                 self.text = _('Fatal error calling ESC_Router()')
         
         # Is an accepted operation?
@@ -142,12 +171,17 @@ class HEI(models.Model):
         if not base_url or not self.productionKey:
             base_url = settings.__dict__.get('ESCROUTER_SANDBOX_URL', None)
             if not base_url or not self.sandboxKey:
-                return None
+                # If the HEI is not connected to the ESC Router, do no harm
+                if operation == 'GET': return None
+                if operation in ['PUT', 'POST']: r.status_code = 201
+                if operation == 'DELETE': r.status_code = 204
+                r.url = 'Success'
+                r.text = 'OK'
+                return r
             headers['key'] = self.sandboxKey
 
         # GET 
         if operation == 'GET':
-            esi = kwargs.get('esi', None)
             if esi is None: return None
             r = requests.get(
                     parse.urljoin(base_url, esi),
@@ -155,8 +189,6 @@ class HEI(models.Model):
                     timeout = settings.__dict__.get(ESCROUTER_TIME_OUT,1)
                 )
             if r.status_code == 200: return r.json()
-
-            if debug: return 'Error response', r.text
 
         if operation == 'PUT':
             if json is None: return None
@@ -216,6 +248,8 @@ class Person(AbstractUser):
                                   on_delete = models.PROTECT,
                                   db_index = True,
                                   default = 1,
+                                  related_name = 'persons',
+                                  related_query_name = 'person',
                                   verbose_name = _('Responsible person'))
     createdOn = models.DateTimeField(verbose_name = _('Created on'),
                                      auto_now_add = True,
@@ -239,10 +273,11 @@ class Person(AbstractUser):
         verbose_name = _('Person')
         verbose_name_plural = _('Persons')
         index_together = ['email', 'identifier']
-        #constraints = [
-        #    models.UniqueConstraint(fields=['email', 'identifier'],
-        #                            name='one_email_per_identifier'),
-        #]
+        ordering = ['last_name', 'first_name']
+        constraints = [
+            models.UniqueConstraint(fields=['email', 'identifier'],
+                                    name='one_email_per_identifier'),
+        ]
 
     def is_officer(self):
         return len(self.heis.all()) > 0
@@ -252,6 +287,88 @@ class Person(AbstractUser):
 
     def has_accepted(self):
         return self.acceptedOn is not None
+
+    def save(self, *args, **kwargs):
+        """
+        We do some checks before saving
+        """
+        # In case someone decides to remove consent, we remove the cards
+        if not self.has_accepted and len(self.cards.all()) > 0:
+            for card in self.cards.all(): card.delete()
+        # If someone has consented and there are unregistered associated cards
+        for card in self.cards.filter(registeredOn__isnull=True):
+            # Send the information to the ESC Router
+            card.save_in_ESCR()
+        super(Person, self).save(*args, **kwargs)
+            
+    def delete(self, *args, **kwargs):
+        """
+        Delete cards before removing the person
+        """
+        for card in self.cards.all(): card.delete()
+        # If there are cards left, something went wrong
+        # we cannot delete the person
+        if not len(self.cards.all()): return
+        super(Person, self).delete(*args, **kwargs)
+            
+
+class Officer(models.Model):
+    """A class for linking persons to the institutions they manage"""
+
+    person = models.ForeignKey(Person,
+                               on_delete = models.DO_NOTHING,
+                               db_index = True, 
+                               related_name = 'heis',
+                               related_query_name = 'hei',
+                               verbose_name = _('Person that manages a HEI'))
+    hei = models.ForeignKey(HEI,
+                            on_delete = models.CASCADE,
+                            db_index = True, 
+                            related_name = 'officers',
+                            related_query_name = 'officer',
+                            verbose_name = _("Managed HEI"))
+
+    # Control information
+    manager = models.ForeignKey(Person,
+                                on_delete = models.PROTECT,
+                                verbose_name = _('Responsible person'))
+    createdOn = models.DateTimeField(_('Created on'),
+                                     auto_now_add = True,
+                                     db_index = True,
+                                     editable = False)
+    modifiedOn = models.DateTimeField(_('Modified on'),
+                                      auto_now = True,
+                                      db_index = True,
+                                      editable = False)
+
+
+    class Meta:
+        verbose_name = _('Officer')
+        verbose_name_plural = _('Officers')
+        index_together = ['person', 'hei']
+        ordering = ['hei', 'person']
+        constraints = [
+            models.UniqueConstraint(fields=['person', 'hei'], name='manager_unique')
+        ]
+
+    def __str__(self):
+        return '{0.person} => {0.hei}'.format(self)
+
+    def delete(self, *args, **kwargs):
+        """
+        Check that officer dependencies have been passed onto other officer
+        """
+        # The replacement will be the first officer from the same HEI
+        replacements = Officer.objects.filter(hei = self.hei)
+        replacements.exclude(person = self.person)
+        # Officer cannot be deleted, here is no replacement
+        if len(replacements) == 0: return
+        # Pass all cards to the first replacement in line
+        for card in self.cards.all(): card.manager = replacements[0]
+        # Pass all persons related to te person that is the deleted officer
+        for person in self.persons.all(): person.managedBy = replacements[0]
+        super(Officer, self).delete(*args, **kwargs)
+
 
 
 class StudentCard(models.Model):
@@ -268,6 +385,9 @@ class StudentCard(models.Model):
                                 verbose_name = _('Student who owns the card'))
     hei = models.ForeignKey(HEI,
                             on_delete = models.CASCADE,
+                            db_index = True, 
+                            related_name = 'studentcards',
+                            related_query_name = 'studentcard',
                             verbose_name = _("Person's HEI"))
     esc = models.CharField(max_length = 300,
                            db_index = True, 
@@ -280,9 +400,11 @@ class StudentCard(models.Model):
                            help_text = 'ESI: European Student Identifier')
 
     # Control information
-    manager = models.ForeignKey(Person,
+    manager = models.ForeignKey(Officer,
                                 on_delete = models.PROTECT,
-                                verbose_name = _('Responsible person'))
+                                related_name = 'cards',
+                                related_query_name = 'card',
+                                verbose_name = _('Responsible officer'))
     createdOn = models.DateTimeField(_('Created on'),
                                      auto_now_add = True,
                                      db_index = True,
@@ -291,9 +413,8 @@ class StudentCard(models.Model):
                                       auto_now = True,
                                       db_index = True,
                                       editable = False)
-    registeredOn = models.DateTimeField(verbose_name = _('Registered on'),
+    registeredOn = models.DateTimeField(verbose_name = _('ESC registered on'),
                                         null = True,
-                                        blank = True,
                                         db_index = True,
                                         editable = False)
 
@@ -302,6 +423,7 @@ class StudentCard(models.Model):
         verbose_name = _('Card')
         verbose_name_plural = _('Cards')
         index_together = ['esi', 'hei', 'student']
+        ordering = ['hei', 'student']
         constraints = [
             models.UniqueConstraint(fields=['esi', 'hei', 'student'],
                                     name='card_unique')
@@ -311,15 +433,37 @@ class StudentCard(models.Model):
         return '{0.student} - {0.esc} - {1}'.format(self, self.myESI())
 
     def save(self, *args, **kwargs):
-        """Check some data and generate what maybe needed"""
+        """
+        Check some data and do whatever maybe needed.
+        """
+        # Do we have an ESC number?
         if self.esc is None or self.esc == '':
             # Generate the ESC
             self.esc = self.hei.generate_esc_code()
-            super(StudentCard, self).save(*args, **kwargs)
+        # Does the card exist in the ESC router? It does if it was registered
+        if self.registeredOn is None and self.student.acceptedOn is not None:
+            # If the person has consented and card is not in the ESCR, save it
+            self.save_in_ESCR()
+        super(StudentCard, self).save(*args, **kwargs)
 
     def is_registered(self):
         return self.registeredOn is not None
 
+    def delete(self, *args, **kwargs):
+        """
+        Clean up in the ESC Router before deleting local information.
+        """
+        if self.is_registered():
+            # Only call the ESC Router if the card has been registered
+            if not self.delete_card():
+                # ESC deletion failed, we can't delete the card
+                return
+            if self.delete_student():
+                # ESC deletion failed, we can't delete the card
+                return
+            self.registered = None
+        super(StudentCard, self).delete(*args, **kwargs)
+            
     def myESI(self, myacid = False):
         """
         Return the European Student Identifier (ESI)
@@ -336,20 +480,20 @@ class StudentCard(models.Model):
 
     def expires(self):
         """
-        Student status expiry date.
+        Student status expiration date.
         ISO 8601 Date Time Format: yyyy-mm-dd:59:59.000X.
         If current month is before term start, year is current year,
         otherwise it is set to the next calendar year.
         Month is the term starting month.
         """
-        now = datetime.date.now()
+        now = timezone.now()
 
         year = now.year
         if now.month > self.hei.termstart: year += 1
 
         day = calendar.monthrange(year, self.hei.termstart)[1]
 
-        expiration = datetime.datetime(year, self.hei.termstart, day,
+        expiration = timezone.datetime(year, self.hei.termstart, day,
                                        23, 59, 00)
         return expiration.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
@@ -372,7 +516,6 @@ class StudentCard(models.Model):
                 u'expiryDate': u'2019-12-31T23:59:59.000Z',
                 u'picInstitutionCode': 999898311,
             }
-        If debug is True, it returns more information.
         """
         return self.hei.ESC_Router('GET', esi = self.myESI())
 
@@ -395,24 +538,21 @@ class StudentCard(models.Model):
                     for x in attributes['cards']]
         return list()
 
-    def save_in_ESCR(self, debug = False):
+    def save_in_ESCR(self):
         """
         Insert or update the card in the European Student Card Router,
         Returns True if everything went well.
         """
         data = {'europeanStudentIdentifier': self.myESI(),
-                'picInstitutionCode': self.PIC_INSTITUCION_CODE,
-                'emailAddress': self.email_address,
-                'expiryDate': expiry_date
+                'picInstitutionCode': self.hei.pic,
+                'emailAddress': self.student.email,
+                'expiryDate': self.expires()
                }
 
         operation = 'POST'
-        if self.card_exists(): operation = 'PUT'
+        if self.is_registered(): operation = 'PUT'
         r = self.hei.ESC_Router(operation, json = data)
-        if r.status_code != 201:
-            if debug:
-                return r.status_code, r.url, 'Insert student error', r.text
-            return False
+        if r.status_code != 201: return False
 
         if self.esc not in self.cards():
             # Add card
@@ -423,28 +563,24 @@ class StudentCard(models.Model):
                 return False
 
         if operation == 'POST': 
-            self.registeredOn = datetime.datetime.now()
-            self.save()
+            self.registeredOn = timezone.now()
 
-        if debug:
-            return r.status_code, r.url, 'Last response', r.text
         return True
 
-    def delete_card(self):
+    def delete_card(self, student=False):
         """
         Removes the information about the card from the ESC Router.
-        If there are no cards left, removes the student.
+        If there are no cards left and student is True, removes the student.
         """
         result = True
-        if self.esc in self.cards():
+        cards = self.cards()
+        if self.esc in cards:
             # Delete card
             r = self.hei.ESC_Router('DELETE',
                                     esi = self.myESI(), esc = self.esc)
-            if r.status_code != 204:
-                if debug:
-                    return r.status_code, r.url, 'Delete card error', r.text
-                return False
-        if len(self.cards()) == 0:
+            if r.status_code != 204: return False
+            cards.remove(self.esc)
+        if student and len(cards) == 0:  # Explicit is better than implict
             # No cards left for the ESI
             result = self.delete_student()
         return result
@@ -454,55 +590,11 @@ class StudentCard(models.Model):
         Removes the information about the student from the ESC Router.
         Only students with no cards can be removed.
         """
-        result = False
         if len(self.cards()) == 0:
             # No cards left for the ESI
             r = self.hei.ESC_Router('DELETE', esi = self.myESI())
-            if debug:
-                return r.status_code, r.url, 'Delete student response', r.text
             return r.status_code == 204
 
-        return result
+        return False
 
-
-class Officer(models.Model):
-    """A class for linking persons to the institutions they manage"""
-
-    person = models.ForeignKey(Person,
-                               on_delete = models.DO_NOTHING,
-                               db_index = True, 
-                               related_name = 'heis',
-                               related_query_name = 'hei',
-                               verbose_name = _('Person that manages a HEI'))
-    hei = models.ForeignKey(HEI,
-                            on_delete = models.CASCADE,
-                            db_index = True, 
-                            related_name = 'managers',
-                            related_query_name = 'manager',
-                            verbose_name = _("Managed HEI"))
-
-    # Control information
-    manager = models.ForeignKey(Person,
-                                on_delete = models.PROTECT,
-                                verbose_name = _('Responsible person'))
-    createdOn = models.DateTimeField(_('Created on'),
-                                     auto_now_add = True,
-                                     db_index = True,
-                                     editable = False)
-    modifiedOn = models.DateTimeField(_('Modified on'),
-                                      auto_now = True,
-                                      db_index = True,
-                                      editable = False)
-
-
-    class Meta:
-        verbose_name = _('Officer')
-        verbose_name_plural = _('Officers')
-        index_together = ['person', 'hei']
-        constraints = [
-            models.UniqueConstraint(fields=['person', 'hei'], name='manager_unique')
-        ]
-
-    def __str__(self):
-        return '{0.person} => {0.hei}'.format(self)
 
--- signals.py
+++ signals.py
@@ -2,22 +2,26 @@
 # vim:ts=4:expandtab:ai
 # $Id$
 """
-   We define functions for receving signals from objects here
+We define functions for receving signals from objects here.
+It has to be imported in models.py, better at the end.
 """
 
 from django.core.signing import TimestampSigner
-from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
+from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_delete, post_delete
 from django.dispatch import receiver
 from django.conf import settings
 from django.core.mail import send_mail,EmailMessage
 
 from .models import Person, StudentCard, HEI
 
+
 # Boilerplate for signal processing code
-#@receiver(signal,sender=Object,dispatch_uid="Name")
-#def emiteRemesa(sender,instance, *args, **kwargs):
+#@receiver(signal, sender = Object, dispatch_uid = "Name")
+#def uniqueFunctionName(sender, instance, *args, **kwargs):
 #    """
 #    Docstring
 #    """
 #    do whatever
 #    return
+
